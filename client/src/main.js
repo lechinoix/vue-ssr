33204e6f9be60c5b6bd3254105d8e244d0d5d78a
lechinoix <nicolas.ngomai@gmail.com>
2019-02-11 23:50:19 +0100

    Add vue-cli client

---
+++ client/src/main.js
@@ -0,0 +1,30 @@
+import Vue from 'vue'
+
+Vue.config.productionTip = false
+
+const componentsToRegister = [
+  {
+    tagName: 'hello-world',
+    componentResolver(resolve) {
+      require(['../../components/HelloWorld.vue'], resolve);
+    }
+  },
+  {
+    tagName: 'slot-component',
+    componentResolver (resolve) {
+      require(['../../components/SlotComponent.vue'], resolve)
+    }
+  }
+];
+
+const registerComponents = () => {
+  componentsToRegister.map(({ tagName, componentResolver }) => {
+    Vue.component(tagName, componentResolver)
+  });
+};
+
+registerComponents();
+
+new Vue({
+  el: '#app-container',
+})
---
+++ components/SlotComponent.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <h1>
+      Some title
+    </h1>
+    <slot></slot>
+  </div>
+</template>
